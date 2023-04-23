30aa55034ece572f122317ba9cc5d31962e0133a
Eris Truong <truongsiphuc2001@gmail.com>
2023-04-24 03:04:18 +0700

    up

--- src/controllers/Api/bookingController.js
+++ src/controllers/Api/bookingController.js
@@ -2,6 +2,37 @@ import db from "../../models/index";
 
 const bookingController = {
 
+  bookingToShipping: async(req, res) => {
+    const id = req.params.id;
+    try {
+      await db.Booking.update({ status: req.params.status }, { where: { id_booking: id }})
+      .then((result) => {
+        if (!result) {
+          return res
+            .status(404)
+            .json({ status: "ERR", msg: "Booking not found" });
+        }
+        return res.status(200).json({
+          status: "OK",
+          data: result,
+        });
+      })
+      .catch((err) => {
+        return res.status(412).json({
+          status: "ERR",
+          msg: err.message,
+        });
+      });
+
+
+    } catch (error) {
+      return res.status(500).json({
+        status: "ERR",
+        msg: error.message,
+      });
+    }
+  },
+
     getAllbookings: async(req, res) => {
       try {
         await db.Booking.findAll()
